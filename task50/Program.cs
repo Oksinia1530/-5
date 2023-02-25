d6e7e59531a87131277f178b9906df7d0429a46b
«Oksana <averinaoy@gmail.com>
2023-02-25 10:55:08 +0500

    task50_1

--- task50/Program.cs
+++ task50/Program.cs
@@ -1,30 +1,44 @@
 ﻿//  Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
 
-Console.WriteLine("Введите размеры массива");
-int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите номер строки");
 int n = Convert.ToInt32(Console.ReadLine());
-int[,] array = new int[m, n];
- 
-for (int i = 0; i < array.GetLength(0); i++) 
+Console.WriteLine("введите номер столбца");
+int m = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+FillArrayRandomNumbers(numbers);
+
+if (n > numbers.GetLength(0) || m > numbers.GetLength(1))
 {
-    for (int j = 0; j < array.GetLength(1); j++)
-        array [i, j] = Convert.ToInt32(new Random().Next(0,21));  
+    Console.WriteLine("такого элемента нет");
 }
- 
-for (int i = 0; i < array.GetLength(0); i++) 
+else
 {
-    for (int j = 0; j < array.GetLength(1); j++)
-        Console.Write(array[i,j] + "\t  ");
-        Console.WriteLine();
+    Console.WriteLine($"значение элемента {n} строки и {m} столбца равно {numbers[n-1,m-1]}");
 }
- 
- Console.WriteLine("Введите координаты");
- int a = Convert.ToInt32(Console.ReadLine());
- int b = Convert.ToInt32(Console.ReadLine());
- if (a>m && b>n)
- Console.WriteLine("такого числа нет");
- else
- {
- object c = array.GetValue(a,b);
- Console.WriteLine(c);
- }
+
+PrintArray(numbers);
+
+void FillArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
